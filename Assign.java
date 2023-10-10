7ec02c10f58fd0455cca94c8c1274da3d5ab8628
Claudio Nuccio <cnuccio00@gmail.com>
2023-10-10 18:08:45 +0200

    Add Assign.java feature

--- Assign.java
+++ Assign.java
@@ -1,7 +1,15 @@
 package Java;
 
 public class Assign {
-    public static void main(String[] args) {
+    public static void myAssign(int x, int y){
+        x  += 6 ;
+        y += 2 ;
 
+        int result = x * y ;
+        System.out.println("The result of x * y after x+6 and y+2 is " + result);
+
+    }
+    public static void main(String[] args) {
+myAssign(12,8);
     }
 }
