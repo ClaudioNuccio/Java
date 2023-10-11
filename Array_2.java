34bc4133f5eb1492bfe470d6aa2707aa72e988bf
Claudio Nuccio <cnuccio00@gmail.com>
2023-10-11 16:52:04 +0200

    Modified Array_2.java

--- Array_2.java
+++ Array_2.java
@@ -5,18 +5,16 @@ import java.util.Random;
 
 public class Array_2 {
     public static void createArray(int x) {
-        int sum = 0;
         char[] characters = new char[x];
         Random r = new Random();
         for (int i = 0; i < x; i++) {
             char c = (char) (r.nextInt(26) + 'a');
             characters[i] = c;
             if (characters[i] == 'a') {
-                sum = sum + 1;
+                System.out.println(characters[i]);
             }
         }
         System.out.println(Arrays.toString(characters));
-        System.out.println("Character 'a' occurred " + sum + " times");
     }
     static public void main (String[]args){
         createArray(15);
