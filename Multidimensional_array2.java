8e296765a75f6c3e11c4c999e92b1819d2991318
Claudio Nuccio <cnuccio00@gmail.com>
2023-10-11 22:50:06 +0200

    Multidimensional_array2.java tries

--- Multidimensional_array2.java
+++ Multidimensional_array2.java
@@ -1,12 +1,12 @@
 
 package Java;
 
+import java.util.Arrays;
+
 public class Multidimensional_array2 {
+
     public static void main(String[] args) {
         int[][] array_1 = {{1, 2, 3}, {4, 5, 6}};
-            for (int i = 0; i < array_1.length; i++) {
-
-                }
 
             }
         }
