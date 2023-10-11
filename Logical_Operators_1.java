fa2500567833bbe2f924a7bfa29177b406ef6ea5
Claudio Nuccio <cnuccio00@gmail.com>
2023-10-11 21:19:56 +0200

    Logical_Operators_1 solved and working on Multidimensional_array2

--- Logical_Operators_1.java
+++ Logical_Operators_1.java
@@ -2,9 +2,13 @@ package Java;
 
 public class Logical_Operators_1 {
     public static void inBetween(int x, int y, int confronto) {
+       boolean compreso = (confronto < x ) && (confronto > y) ;
+       System.out.println(confronto + " is between " + x + " and " + y);
     }
 
+
     public static void main(String[] args) {
+        inBetween(6,4,5);
     }
 }
 
--- Multidimensional_array2.java
+++ Multidimensional_array2.java
@@ -2,19 +2,11 @@
 package Java;
 
 public class Multidimensional_array2 {
+    public static void main(String[] args) {
+        int[][] array_1 = {{1, 2, 3}, {4, 5, 6}};
+            for (int i = 0; i < array_1.length; i++) {
 
-    public static int [] [] rearrange (int [] [] x){
-        int array_1 [] [] = new int[x.length][x.length];
-        {for (int i = 0; i < x.length; ++i) {
-            for (int j = 0; j < x[0].length; ++j) {
-                array_1[j][i] = x[i][j];
+                }
+
             }
         }
-        return array_1;
-        }
-    }
-    public static void main (String [] args){
-
-    }
-}
-
