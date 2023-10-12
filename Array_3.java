2c81f74a1d945624675bfab44acb013f3336bd4e
Claudio Nuccio <cnuccio00@gmail.com>
2023-10-12 13:04:31 +0200

    Fixed Array_3.java

--- Array_3.java
+++ Array_3.java
@@ -4,23 +4,55 @@ import java.util.Arrays;
 import java.util.LinkedHashSet;
 import java.util.Set;
 public class Array_3 {
-    public static int removeDup(int arrayDupes[], int length){
-        if (length==0 || length==1){
-            return length;
+    public static int[] removeDup(int[] arrayDupes, int length) {
+        if (length == 0 || length == 1)
+        {
+            System.out.println("This Array doesn't have any dupes because it's too small");
+            System.out.println(Arrays.toString(arrayDupes));
+            return arrayDupes;
         }
-        int[] noDupes = new int[length];
-        int j = 0;
-        for (int i=0; i<length-1; i++){
-            if (arrayDupes[i] != arrayDupes[i+1]){
-                noDupes[j++] = arrayDupes[i];
+            int temp[] = new int[length];
+            int index = 0;
+            int dupes = 0;
+            boolean change;
+            boolean zero = true;
+            for (int i = 0; i < length; i++) {
+                change = true;
+                for (int j = 0; j < length; j++) {
+                    if (arrayDupes[i] == 0 && zero) {
+                        zero = false;
+                        dupes--;
+                    } else if (arrayDupes[i] == 0 && !zero) {
+                        change = false;
+                    } else if (arrayDupes[i] == temp[j]) {
+                        change = false;
+                    }
+
+                }
+                if (change) {
+                    temp[index++] = arrayDupes[i];
+                } else {
+                    dupes++;
+                }
             }
+            int[] def = new int[length - dupes];
+            for (int i = 0; i < def.length; i++) {
+                def[i] = temp[i];
+            }
+            System.out.println(Arrays.toString(def));
+            return temp;
         }
-        noDupes[j++] = arrayDupes[length-1];
-        for (int i=0; i<j; i++){
-            arrayDupes[i] = noDupes[i];
-        }
-        return j;
+
+     public static void main(String[] args) {
+        int [] dupes = {1,2,3,3,4,5,5,6,7,8,9,9,11,12,13,14,15,0,0};
+        int length = dupes.length;
+        int [] zeros = {0};
+        removeDup(dupes, length);
+        removeDup(zeros, zeros.length);
     }
+
+}
+
     // public static void removeDupes(int[] array) {
       //  Set<Integer> noDupes = new LinkedHashSet<Integer>();
        // for (int i = 0; i < array.length; i++) {
@@ -29,17 +61,8 @@ public class Array_3 {
     //ArrayList<Integer> arrayNoDupes = new ArrayList<>(noDupes);
        // System.out.println(arrayNoDupes);
    // }
-    public static void main (String []args){
-        int [] arrayOne = {1,1,2,3,3,4,5,6,7,7,8,9,9};
-        Arrays.sort(arrayOne);
-        int length = arrayOne.length;
-        length = removeDup(arrayOne,length);
-        for (int i=0; i<length; i++) {
-            System.out.print(arrayOne[i] + " ");
-        }
 
-    }
-}
+
 
 
 
