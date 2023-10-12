f8035d57487798e048255659f234f43fefb52e13
Claudio Nuccio <cnuccio00@gmail.com>
2023-10-12 11:52:48 +0200

    Array_3.java without sets

--- Array_3.java
+++ Array_3.java
@@ -1,20 +1,43 @@
 package Java;
-
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedHashSet;
 import java.util.Set;
-
 public class Array_3 {
-    public static void removeDupInIntArray(int[] array) {
-        Set<Integer> noDupes = new LinkedHashSet<Integer>();
-        for (int i = 0; i < array.length; i++) {
-            noDupes.add(array [i]);
+    public static int removeDup(int arrayDupes[], int length){
+        if (length==0 || length==1){
+            return length;
         }
-        System.out.println(noDupes);
+        int[] noDupes = new int[length];
+        int j = 0;
+        for (int i=0; i<length-1; i++){
+            if (arrayDupes[i] != arrayDupes[i+1]){
+                noDupes[j++] = arrayDupes[i];
+            }
+        }
+        noDupes[j++] = arrayDupes[length-1];
+        for (int i=0; i<j; i++){
+            arrayDupes[i] = noDupes[i];
+        }
+        return j;
     }
+    // public static void removeDupes(int[] array) {
+      //  Set<Integer> noDupes = new LinkedHashSet<Integer>();
+       // for (int i = 0; i < array.length; i++) {
+         //   noDupes.add(array [i]);
+       // }
+    //ArrayList<Integer> arrayNoDupes = new ArrayList<>(noDupes);
+       // System.out.println(arrayNoDupes);
+   // }
     public static void main (String []args){
         int [] arrayOne = {1,1,2,3,3,4,5,6,7,7,8,9,9};
-     removeDupInIntArray(arrayOne);
+        Arrays.sort(arrayOne);
+        int length = arrayOne.length;
+        length = removeDup(arrayOne,length);
+        for (int i=0; i<length; i++) {
+            System.out.print(arrayOne[i] + " ");
+        }
+
     }
 }
 
