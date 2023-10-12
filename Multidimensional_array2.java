c8feb95e0dd81241f2535ad43d88828ecc93878e
Claudio Nuccio <cnuccio00@gmail.com>
2023-10-12 10:28:54 +0200

    Multidimensional_array2.java fixed

--- Multidimensional_array2.java
+++ Multidimensional_array2.java
@@ -4,9 +4,17 @@ package Java;
 import java.util.Arrays;
 
 public class Multidimensional_array2 {
-
-    public static void main(String[] args) {
-        int[][] array_1 = {{1, 2, 3}, {4, 5, 6}};
-
-            }
+    public static void rowsAndColumns(int[] [] array) {
+       for (int column = 0 ; column < array[0].length;column++){
+           for (int row = 0 ; row < array.length; row ++){
+               System.out.print(array[row][column]+"\t");
+           }
+           System.out.println();
+       }
         }
+    public static void main (String[] args){
+int [] [] multiOne = {{1,2,3},
+                      {4,5,6,}};
+rowsAndColumns(multiOne);
+    }
+}
