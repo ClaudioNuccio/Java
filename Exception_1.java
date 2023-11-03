30630ca06bb59a653dc98ceabbca27e96b9a0ac2
Claudio Nuccio <cnuccio00@gmail.com>
2023-11-03 12:20:03 +0100

    Fixed Exception_1

--- Exception_1.java
+++ Exception_1.java
@@ -1,21 +1,26 @@
 package Java;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Exception_1 {
-    public static void myException (int limiteSuperiore, int limiteInferiore) {
+    public static void myException(int limiteSuperiore, int limiteInferiore) {
         try {
             Scanner scanner = new Scanner(System.in);
             System.out.println("Dammi un numero per controllare se è nel raggio selezionato");
             int check = scanner.nextInt();
 
-            if (check <= limiteSuperiore && check >= limiteInferiore) {System.out.println(true);
+            if (check <= limiteSuperiore && check >= limiteInferiore) {
+                System.out.println(true);
             }
-            if (check > limiteSuperiore || check < limiteInferiore){
-               throw new RuntimeException("Eccezione1");
+            if (check > limiteSuperiore || check < limiteInferiore) {
+                throw new ArithmeticException("Eccezione1");
             }
-            } catch (RuntimeException e){
+        } catch (ArithmeticException e) {
             System.out.println("Il numero inserito è fuori dal range desiderato");
+        }
+        catch (InputMismatchException e){
+            System.out.println("Non hai immesso un numero");
         }
     }
 
