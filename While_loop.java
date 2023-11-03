63557a790ff81ba97c54048f55fb117d9a5a1164
Claudio Nuccio <cnuccio00@gmail.com>
2023-11-03 11:43:44 +0100

    fixed while

--- While_loop.java
+++ While_loop.java
@@ -2,7 +2,7 @@ package Java;
 
 public class While_loop {
 
-    public static void mySumWhile(String[] args){
+    public static void mySumWhile(){
         int i = 0 ;
         int sum = 0 ;
         while (i < 10){
