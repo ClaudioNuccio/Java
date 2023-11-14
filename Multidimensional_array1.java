8d534c50200681544f289d122739912ecf0e863b
Claudio Nuccio <cnuccio00@gmail.com>
2023-11-14 12:47:43 +0100

    fixed

--- Multidimensional_array1.java
+++ Multidimensional_array1.java
@@ -1,10 +1,10 @@
 package Java;
 import java.util.Random;
 public class Multidimensional_array1 {
-        public static void createMultiArray () {
+        public static void createMultiArray (int x) {
             int sum = 0;
             Random r = new Random();
-            int[][] a = new int[3][3];
+            int[][] a = new int[x][x];
             for (int i = 0; i < a.length; i++) {
                 for (int j = 0; j < a.length; j++) {
                     a[i][j] = r.nextInt(10);
@@ -16,6 +16,10 @@ public class Multidimensional_array1 {
                 sum += a[0][k];
             }
             System.out.println("The sum of the first lane of this multidimensional array is = " + sum);
+        }
+
+        public static void main (String [] args){
+            createMultiArray(3);
         }
     }
 
--- While_loop.java
+++ While_loop.java
@@ -2,13 +2,16 @@ package Java;
 
 public class While_loop {
 
-    public static void mySumWhile(){
+    public static void mySumWhile(int x){
         int i = 0 ;
         int sum = 0 ;
-        while (i < 10){
+        while (i < x){
             sum =sum + i ;
             i++;
         }
         System.out.println(sum);
+    }
+    public static void main (String [] args) {
+        mySumWhile(15);
     }
 }
