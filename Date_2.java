5fc70dc9adaab8413e427f11c0ef68d0c9afaf2a
Claudio Nuccio <cnuccio00@gmail.com>
2023-11-17 11:56:47 +0100

    Fixed Date_2

--- Date_2.java
+++ Date_2.java
@@ -2,11 +2,12 @@ package Java;
 
 import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 public class Date_2 {
     public static void main(String[] args) {
         OffsetDateTime exercise = OffsetDateTime.parse("2023-03-01T13:00:00Z");
-       String exerciseSolved = exercise.format(DateTimeFormatter.ofPattern("dd MMMM yyyy"));
+       String exerciseSolved = exercise.format(DateTimeFormatter.ofPattern("dd MMMM yyyy", Locale.ITALY));
         System.out.println(exerciseSolved);
     }
 }
