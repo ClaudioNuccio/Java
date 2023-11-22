878081b73d9185ddb2deb4508da56fb4a115663d
Claudio Nuccio <cnuccio00@gmail.com>
2023-11-22 15:50:05 +0100

    Fixed Ternary_2

--- Ternary_2.java
+++ Ternary_2.java
@@ -2,14 +2,14 @@ package Java;
 
 public class Ternary_2 {
 
-        public static void main(String[] args) {
-            for (int i = 1; i <= 100; i++) {
-                System.out.println("Valore: " + i);
-                System.out.println(checkValue(i));
-            }
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println("Valore: " + i);
+            System.out.println(checkValue(i));
         }
+    }
 
-        private static String checkValue(int value) {
-            String result = value % 5 == 0 ? "FizzBuzz" : "Buzz";
-        return result;
-    }}
+    private static String checkValue(int value) {
+        return value % 5 == 0 ? "FizzBuzz" : "Buzz";
+    }
+}
